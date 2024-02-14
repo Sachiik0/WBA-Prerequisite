4cf41f7f1afecfc893479d61d84da229948a699e
Marc Anthony San Juan <118324870+Sachiik0@users.noreply.github.com>
2024-02-15 00:32:52 +0800

    Update recover.js

--- recover.js
+++ recover.js
@@ -1,6 +1,6 @@
 
 const bs58 = require('bs58');
 const fs = require('fs');
-b = bs58.decode('4KXvjggpxf1jquaMePAjyfJDpqHdqVo9ZRiezFd1JRrDZbNWcaQJUAAW38n7Vx4MRRy3qgww7cmXKb3E6c8ELf4u');
+b = bs58.decode('publickey');
 j = new Uint8Array(b.buffer, b.byteOffset, b.byteLength / Uint8Array.BYTES_PER_ELEMENT);
 fs.writeFileSync('key.json', `[${j}]`);
