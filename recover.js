25c5003d6c7e074632e020bec012c226f89df411
Marc Anthony San Juan <118324870+Sachiik0@users.noreply.github.com>
2024-02-15 00:36:43 +0800

    Delete recover.js

--- recover.js
+++
@@ -1,6 +0,0 @@
-
-const bs58 = require('bs58');
-const fs = require('fs');
-b = bs58.decode('publickey');
-j = new Uint8Array(b.buffer, b.byteOffset, b.byteLength / Uint8Array.BYTES_PER_ELEMENT);
-fs.writeFileSync('key.json', `[${j}]`);
